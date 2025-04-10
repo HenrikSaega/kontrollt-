c99eaeaae4b884999718467e8333806c50db9c36
Henrik Saega <Henriksaega@gmail.com>
2025-04-10 20:13:25 +0300

    Konsool väljastab JSON andmed

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const fs = require("fs/promises");
+const bodyParser = require("body-parser")
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get("/meals", async (req, res) => {
+  const mealsPath = path.join(__dirname, "data", "meals.json");
+  //console.log("/meals töötab")
+
+  const data = await fs.readFile(mealsPath, "utf-8");
+  console.log("Meals JSON andmed:", data);
+  const meals = JSON.parse(data)
+  res.json(JSON.parse(data));
+});
+
+app.use((req, res) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  res.status(404).json({ message: "Not found" });
+});
+
+app.listen(3000); // Changed 3001 => 3000
